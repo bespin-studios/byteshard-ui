6216181f4df51f0f2b704de6bb01625b43d373df
Lars Hennig <lars@bespin-studios.com>
2024-04-24 13:06:41 +0200

    fix: check if app config implements oauth interface

--- src/public/login/oauth.php
+++ src/public/login/oauth.php
@@ -1,5 +1,6 @@
 <?php
 
+use byteShard\Config\OauthInterface;
 use byteShard\Internal\Authentication\Authentication;
 use byteShard\Internal\Authentication\Providers;
 use byteShard\Internal\Server;
@@ -15,13 +16,17 @@ if (isset($env) && ($env instanceof Environment)) {
     $env->startSession();
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET' && array_key_exists('code', $_GET)) {
-        $oauth         = new \byteShard\Internal\Authentication\Provider\Oauth($env->getOauthProvider(), $env->getJwksCertPath());
-        $authenticated = $oauth->authenticate();
-        if ($authenticated === true) {
-            Authentication::setAuthenticationProviderCookie(Providers::OAUTH);
-            $env->processSuccessfulLogin($oauth->getUsername());
-            header('Location: '.Server::getBaseUrl());
-            exit;
+        if ($env instanceof OauthInterface) {
+            $oauth         = new \byteShard\Internal\Authentication\Provider\Oauth($env->getOauthProvider(), $env->getJwksCertPath());
+            $authenticated = $oauth->authenticate();
+            if ($authenticated === true) {
+                Authentication::setAuthenticationProviderCookie(Providers::OAUTH);
+                $env->processSuccessfulLogin($oauth->getUsername());
+                header('Location: '.Server::getBaseUrl());
+                exit;
+            }
+        } else {
+            \byteShard\Debug::debug('Oauth login requires App\Config\ByteShard.php to implement the '.OauthInterface::class);
         }
     }
 }
